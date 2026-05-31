a5e6c26964df934f8526983c8ed9df9fd9877129
Oguzhan <seninmailin@gmail.com>
2026-05-31 09:30:29 +0300

    Add Meta Pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,6 +57,39 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     `,
   }}
 />
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s);
+            }(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1807355850237399');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1807355850237399&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <Script id="tawk-live-chat" strategy="afterInteractive">
           {`
             var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
